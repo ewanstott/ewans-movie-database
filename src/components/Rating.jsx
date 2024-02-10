3f943a8db32afc24604f0ddda38d3e51962c1ddc
Ewan Stott <ewan.stott1@gmail.com>
2024-02-10 16:13:01 +0000

    star icons added

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { FaRegStar } from "react-icons/fa";
+import { FaRegStarHalf } from "react-icons/fa6";
 
 class Rating extends Component {
   render() {
@@ -7,13 +8,28 @@ class Rating extends Component {
 
     console.log(Math.floor(voteAverage));
 
+    //create array where length matches count of votes
+    const count = [];
+    for (let i = 0; i < voteAverage; i++) {
+      count.push("");
+    }
+
     return (
       <>
         <p>Total Votes: {voteCount}</p>
         <p>Rating: {voteAverage}</p>
         {new Array(Math.floor(voteAverage)).fill("").map(() => {
-          return <FaRegStar />;
+          return (
+            <span className="star">
+              <FaRegStar />
+            </span>
+          );
         })}
+        {voteAverage !== Math.round(voteAverage) && (
+          <span className="star">
+            <FaRegStarHalf />
+          </span>
+        )}
       </>
     );
   }
